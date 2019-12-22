88d4222b28fe9650e6ab07a84bfebe6a59adbd89
忘却的旋律 <fhenn@163.com>
2019-12-22 21:41:48 +0800

    新增apache tiles视图解析器和thymeleaf视图解析器

--- src/main/java/com/melody/config/spitter/WebConfig.java
+++ src/main/java/com/melody/config/spitter/WebConfig.java
@@ -8,8 +8,12 @@ import org.springframework.web.servlet.config.annotation.DefaultServletHandlerCo
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
-import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
 
 /**
  * @author 40431
@@ -41,23 +45,62 @@ public class WebConfig extends WebMvcConfigurerAdapter {
      * 配置Apache Tiles视图解析器
      * @return tiles
      */
-    @Bean
-    public TilesConfigurer tilesConfigurer(){
-        TilesConfigurer tiles = new TilesConfigurer();
-        // 指定tiles定义的位置
-        tiles.setDefinitions(new String[]{"/WEB-INF/layout/tiles.xml"});
-        // 启用刷新功能
-        tiles.setCheckRefresh(true);
-        return tiles;
-    }
+//    @Bean
+//    public TilesConfigurer tilesConfigurer(){
+//        TilesConfigurer tiles = new TilesConfigurer();
+//        // 指定tiles定义的位置
+//        tiles.setDefinitions(new String[]{"/WEB-INF/layout/tiles.xml"});
+//        // 启用刷新功能
+//        tiles.setCheckRefresh(true);
+//        return tiles;
+//    }
 
     /**
      * tilesViewResolver apache tiles视图解析器
      * @return TilesViewResolver
      */
+//    @Bean
+//    public ViewResolver viewResolver(){
+//        return new TilesViewResolver();
+//    }
+
+    /**
+     * 设置视图解析器
+     * @param templateEngine
+     * @return ThymeleafViewResolver
+     */
     @Bean
-    public ViewResolver viewResolver(){
-        return new TilesViewResolver();
+    public ViewResolver viewResolver(SpringTemplateEngine templateEngine){
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine);
+        return resolver;
+    }
+
+    /**
+     * 设置模板引擎
+     * @param templateResolver
+     * @return SpringTemplateEngine
+     */
+    @Bean
+    public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver){
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(templateResolver);
+        return engine;
+    }
+
+    /**
+     * 模板解析引擎
+     * @return SpringResourceTemplateResolver
+     */
+    @Bean
+    public SpringResourceTemplateResolver templateResolver(){
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("/WEB-INF/template/");//设置地址前缀
+        resolver.setSuffix(".html");//设置后缀
+        resolver.setCacheable(false);//设置缓存
+        resolver.setTemplateMode(TemplateMode.HTML);
+        return resolver;
+
     }
 
     /**
