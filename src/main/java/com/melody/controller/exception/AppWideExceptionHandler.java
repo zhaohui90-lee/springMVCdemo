21a5339df7c9dc243677aafb638d5e337e1a615f
zhaohui90-lee <fhenn@163.com>
2020-01-03 13:42:09 +0800

    自定义CustomerNotFoundException

--- src/main/java/com/melody/controller/exception/AppWideExceptionHandler.java
+++ src/main/java/com/melody/controller/exception/AppWideExceptionHandler.java
@@ -1,13 +1,19 @@
 package com.melody.controller.exception;
 
+import com.melody.controller.pizza.service.CustomerNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.io.IOException;
+
+/**
+ * @author 40431
+ */
 @ControllerAdvice
 public class AppWideExceptionHandler {
 
     @ExceptionHandler
-    public String duplicateSpittleHandler(){
+    public String duplicateSpittleHandler(CustomerNotFoundException cus){
         return "error/duplicate";
     }
 }
