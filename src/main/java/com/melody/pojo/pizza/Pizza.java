29142e543f013dbcc00b6238a3d82d3788466df2
zhaohui90-lee <fhenn@163.com>
2020-01-03 13:40:53 +0800

    将java类配置位spring bean

--- src/main/java/com/melody/pojo/pizza/Pizza.java
+++ src/main/java/com/melody/pojo/pizza/Pizza.java
@@ -1,9 +1,12 @@
 package com.melody.pojo.pizza;
 
+import org.springframework.stereotype.Component;
+
 import java.io.Serializable;
 
 /**
  * @author 40431
  */
+@Component
 public class Pizza implements Serializable {
 }
