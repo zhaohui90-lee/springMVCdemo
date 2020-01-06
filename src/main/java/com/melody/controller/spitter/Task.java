74f2904d8022efe7d97c63dccf560b17685a53e7
zhaohui90-lee <fhenn@163.com>
2020-01-06 11:14:39 +0800

    多线程

---
+++ src/main/java/com/melody/controller/spitter/Task.java
@@ -0,0 +1,26 @@
+package com.melody.controller.spitter;
+
+public class Task implements Runnable{
+
+    private int i;
+
+    public Task(int i) {
+        this.i = i;
+    }
+
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+
+    }
+}
