ae8dde8d3d61d42923526c7700d08863d981f7e7
忘却的旋律 <fhenn@163.com>
2020-02-08 10:01:45 +0800

    spring bean的继承

---
+++ src/main/java/com/melody/pojo/news/NewsProviderTemplate.java
@@ -0,0 +1,5 @@
+package com.melody.pojo.news;
+
+abstract class NewsProviderTemplate {
+
+}
---
+++ src/main/java/com/melody/pojo/news/SpecificFXNewsProvider.java
@@ -0,0 +1,31 @@
+package com.melody.pojo.news;
+
+public class SpecificFXNewsProvider extends FXNewsProvider{
+    private IFXNewsListener newsListener;
+    private IFXNewsPersistener newsPersistener;
+
+    public SpecificFXNewsProvider() {
+    }
+
+    public SpecificFXNewsProvider(IFXNewsListener newsListener, IFXNewsPersistener newsPersistener) {
+        super(newsListener, newsPersistener);
+    }
+
+    public IFXNewsListener getNewsListener() {
+        return newsListener;
+    }
+
+    @Override
+    public void setNewsListener(IFXNewsListener newsListener) {
+        this.newsListener = newsListener;
+    }
+
+    public IFXNewsPersistener getNewsPersistener() {
+        return newsPersistener;
+    }
+
+    @Override
+    public void setNewsPersistener(IFXNewsPersistener newsPersistener) {
+        this.newsPersistener = newsPersistener;
+    }
+}
