086c9cbe4a6e275bca2201a042eeb38f5d21e837
忘却的旋律 <fhenn@163.com>
2020-02-22 19:21:58 +0800

    算法：基础编程模型

--- src/main/java/com/melody/util/Algorithms.java
+++ src/main/java/com/melody/util/Algorithms.java
@@ -108,18 +108,23 @@ public class Algorithms {
 //            StdDraw.point(i,i*Math.log(i));
 //        }
 
-        int N = 50;
-        double[] a = new double[N];
-        for (int i = 0; i < N; i++) {
-            a[i] = StdRandom.uniform();
-        }
-        Arrays.sort(a);
-        for (int i = 0; i < N; i++) {
-            double x = 1.0*i/N;
-            double y = a[i]/2.0;
-            double rw = 0.5/N;
-            double rh = a[i]/2.0;
-            StdDraw.filledRectangle(x,y,rw,rh);
-        }
+//        int N = 50;
+//        double[] a = new double[N];
+//        for (int i = 0; i < N; i++) {
+//            a[i] = StdRandom.uniform();
+//        }
+//        Arrays.sort(a);
+//        for (int i = 0; i < N; i++) {
+//            double x = 1.0*i/N;
+//            double y = a[i]/2.0;
+//            double rw = 0.5/N;
+//            double rh = a[i]/2.0;
+//            StdDraw.filledRectangle(x,y,rw,rh);
+//        }
+
+        System.out.println(Math.abs(-2147483648)); // -2147483648
+//        System.out.println(1/0);
+//        System.out.println(1.0/0.0);
+        System.out.println("asdad\\s+sss");
     }
 }
---
+++ src/main/java/com/melody/util/VisualAccumulator.java
@@ -0,0 +1,40 @@
+package com.melody.util;
+
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class VisualAccumulator {
+    private double total;
+    private int N;
+
+    public VisualAccumulator(int trials,double max) {
+        StdDraw.setXscale(0,trials);
+        StdDraw.setYscale(0,max);
+        StdDraw.setPenRadius(.005);
+    }
+    public void addDataValue(double val){
+        N++;
+        total += val;
+        StdDraw.setPenColor(StdDraw.DARK_GRAY);
+        StdDraw.point(N,val);
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.point(N,total/N);
+    }
+    public double mean(){
+        return total/N;
+    }
+    public String toString(){
+        return "Mean (" + N + " values): "
+                + String.format("%7.5f",mean());
+    }
+
+    public static void main(String[] args) {
+        int T = Integer.parseInt("2000");
+        VisualAccumulator a = new VisualAccumulator(T,1.0);
+        for (int i = 0; i < T; i++) {
+            a.addDataValue(StdRandom.uniform());
+        }
+        StdOut.println(a);
+    }
+}
