c9eccdc111d23d848e035f7fd87ae5a0832ac6ec
忘却的旋律 <fhenn@163.com>
2020-02-23 14:28:03 +0800

    算法：可变长度数组

---
+++ src/test/java/collection/FixedCapacityStack.java
@@ -0,0 +1,40 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FixedCapacityStack<Item> {
+    private Item[] a;
+    private int N;
+
+    public FixedCapacityStack(int cap) {
+        a = (Item[]) new Object[cap];
+    }
+    public boolean isEmpty(){
+        return N == 0;
+    }
+    public int size(){
+        return N;
+    }
+    public void push(Item item){
+        if (N == a.length) resize(2*a.length);
+        a[N++] = item;
+    }
+
+    private void resize(int max) {
+        // 将大小为N<=max的栈移动到一个新的大小为max的数组中
+        Item[] temp = (Item[]) new Object[max];
+        for (int i = 0; i < N; i++) {
+            temp[i] = a[i];
+        }
+        a = temp;
+    }
+
+    public Item pop(){
+        // 从栈定删除元素
+        Item item = a[--N];
+        a[N] = null;// 避免对象游离
+        if (N > 0 && N == a.length/4) resize(a.length/2);
+        return item;
+    }
+}
