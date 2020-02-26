07e66565d76803119cf5dd3a1cda48fec0699b65
zhaohui90-lee <fhenn@163.com>
2020-02-26 15:33:04 +0800

    算法：三向切分的快速排序，主要优化了元素重复时的效率

--- src/test/java/sort/Quick3way.java
+++ src/test/java/sort/Quick3way.java
@@ -1,5 +1,43 @@
 package sort;
 
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
+
 public class Quick3way {
 
+    public static void sort(Comparable[] a){
+        StdRandom.shuffle(a);
+        sort(a,0,a.length-1);
+    }
+
+    private static void sort(Comparable[] a,int lo,int hi){
+        if (hi <= lo) return;
+        int lt = lo,i = lo + 1,gt = hi;
+        Comparable v = a[lo];
+        while (i <= gt){
+            int cmp = a[i].compareTo(v);
+            if (cmp < 0) exch(a,lt++,i++);
+            else if (cmp > 0) exch(a,i,gt--);
+            else i++;
+        }
+        sort(a,lo,lt-1);
+        sort(a,gt+1,hi);
+    }
+
+    private static void exch(Comparable[] a,int i,int j){
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    public static void main(String[] args) {
+        Double[] a = new Double[100000];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = StdRandom.uniform();
+        }
+        Stopwatch timer = new Stopwatch();
+        sort(a); // time: 0.135 seconds time: 0.148 seconds time: 0.321 seconds time: 0.128 seconds time: 0.177 seconds
+        double time = timer.elapsedTime();
+        System.out.println("time: " + time + " seconds");
+    }
 }
