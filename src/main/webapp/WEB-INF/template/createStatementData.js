277a1c92fac9e644e4ce201ccac08c2543e1c38c
zhaohui90-lee <fhenn@163.com>
2020-03-13 16:05:11 +0800

    重构代码:按照类型重组计算

--- src/main/webapp/WEB-INF/template/createStatementData.js
+++ src/main/webapp/WEB-INF/template/createStatementData.js
@@ -1,10 +1,68 @@
-function createStatementData(invoice,plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalAmountCredits = totalVolumeCredits(statementData);
-    return statementData;
+export default function createStatementData(invoice,plays) {
+    const result = {};
+    result.customer = invoice.customer;
+    result.performances = invoice.performances.map(enrichPerformance);
+    result.totalAmount = totalAmount(result);
+    result.totalAmountCredits = totalVolumeCredits(result);
+    return result;
+
+    function enrichPerformance(aPerformance) {
+        const caculator = new PerformanceCalculator(aPerformance,playFor(aPerformance));
+        const result = Object.assign({},aPerformance);
+        result.play = playFor(result);
+        result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
+        return result;
+    }
+
+    class PerformanceCalculator{
+        constructor(aPerformance,aPlay) {
+            this.performance = aPerformance;
+            this.play = aPlay;
+        }
+
+        get amount(){
+            throw new Error(`unknown type:${playFor(aPerformance).type}`);
+        }
+
+        get volumeCredits(){
+            return Math.max(aPerformance.audience - 30,0);;
+        }
+    }
+
+    class TragedyCalculator extends PerformanceCalculator{
+        get amount(){
+            let result = 40000;
+            if (this.performance.audience > 30){
+                result += 1000 * (this.performance.audience - 30);
+            }
+            return result;
+        }
+    }
+
+    class ComedyCalculator extends PerformanceCalculator{
+        get amount(){
+            let result = 30000;
+            if (this.performance.audience > 20){
+                result += 10000 + 500 * (this.performance.audience - 20);
+            }
+            result += 300 * this.performance.audience;
+            return result;
+        }
+
+        get volumeCredits(){
+            return super.volumeCredits + Math.floor(aPerformance.audience / 5);
+        }
+    }
+
+    function createPerformanceCalculator(aPerformance,aPlay) {
+        switch (aPlay.type) {
+            case "tragedy": return new TragedyCalculator(aPerformance,aPlay);
+            case "comedy": return new ComedyCalculator(aPerformance,aPlay);
+            default:
+                throw new Error(`unknown type:${aPlay.type}`);
+        }
+    }
 
     function totalAmount(data) {
         let result = 0;
@@ -23,47 +81,15 @@ function createStatementData(invoice,plays) {
         return volumeCredits;
     }
 
-    function enrichPerformance(aPerformance) {
-        const result = Object.assign({},aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
-        return result;
-    }
-
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }
 
     function amountFor(aPerformance) {
-        let result = 0;
-
-        switch (aPerformance.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30){
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20){
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`unknown type:${playFor(aPerformance).type}`);
-        }
-        return result;
+        return new PerformanceCalculator(aPerformance,playFor(aPerformance)).amount;
     }
 
     function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        //  add volume credits
-        result += Math.max(aPerformance.audience - 30,0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-        return result;
+        return new PerformanceCalculator(aPerformance,playFor(aPerformance)).volumeCredits;
     }
 }
--- src/main/webapp/WEB-INF/template/statement.js
+++ src/main/webapp/WEB-INF/template/statement.js
@@ -1,4 +1,4 @@
-import createStatementData from './createStatementData';
+import createStatementData from './createStatementData.js';
 
 function statement(invoice,plays) {
     return renderPlainText(createStatementData(invoice,plays));
