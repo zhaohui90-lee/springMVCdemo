5be707857972b534bd452baaeb54e4b2715c1792
忘却的旋律 <fhenn@163.com>
2020-03-26 21:23:46 +0800

    per-instance类型的aop

---
+++ src/main/java/com/melody/aop/perInstance/ITester.java
@@ -0,0 +1,9 @@
+package com.melody.aop.perInstance;
+
+/**
+ * @Author melody
+ * @Date 2020-03-26
+ */
+public interface ITester {
+
+}
---
+++ src/main/java/com/melody/aop/perInstance/TesterFeatureIntroductionInterceptor.java
@@ -0,0 +1,29 @@
+package com.melody.aop.perInstance;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.aop.support.DelegatingIntroductionInterceptor;
+
+/**
+ * @Author melody
+ * @Date 2020-03-26
+ */
+public class TesterFeatureIntroductionInterceptor extends DelegatingIntroductionInterceptor implements ITester {
+    private static final long serialVersionUID = 1L;
+    private boolean busyAsTester;
+
+    public Object invoke(MethodInvocation mi) throws Throwable {
+        if (isBusyAsTester() && StringUtils.contains(mi.getMethod().getName(),"developSoftware")){
+            throw new RuntimeException("12313211");
+        }
+        return super.invoke(mi);
+    }
+
+    public boolean isBusyAsTester() {
+        return busyAsTester;
+    }
+
+    public void setBusyAsTester(boolean busyAsTester) {
+        this.busyAsTester = busyAsTester;
+    }
+}
